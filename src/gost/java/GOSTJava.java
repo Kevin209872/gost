df1d0e6f4cc055dacf138658cfaf42af9cdd5585
Kevin209872 <94930133+Kevin209872@users.noreply.github.com>
2021-11-23 14:38:04 -0600

    Update GOSTJava.java

--- src/gost/java/GOSTJava.java
+++ src/gost/java/GOSTJava.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gost.java;
 
-/**
- *
- * @author KEVIN
- */
 public class GOSTJava {
 
     /**
@@ -16,6 +8,7 @@ public class GOSTJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println
     }
     
 }
